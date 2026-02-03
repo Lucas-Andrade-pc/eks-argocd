6c16c04093db27f310735082c9c0155478cfe7a4
Lucas Andrade de Oliveira <lucas.oliveira@irede.org.br>
2026-02-03 14:12:25 -0300

    enable endpoint private

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -28,7 +28,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks.arn
 
   vpc_config {
-    endpoint_private_access = false
+    endpoint_private_access = true
     endpoint_public_access  = true
 
     subnet_ids = [
